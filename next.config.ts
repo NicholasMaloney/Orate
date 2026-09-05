59a6b714d5030a8fa91d5a1a5639a05077bcd544
Nicholas Maloney <Nicholas.Biz@protonmail.com>
2026-09-05 13:36:37 +1000

    enable standalone output, ground work for docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* docker here */
+  output: "standalone",
 };
 
 export default nextConfig;
